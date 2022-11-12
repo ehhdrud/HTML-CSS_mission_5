781b70c2004bebd4f0918677b44d89382c3e1edf
서동경 <ehhdrud@icloud.com>
2022-11-13 04:52:58 +0900

    221113

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,7 @@
-import { Menubar } from './Menubar/Menubar.js';
+import { Menubar } from "./Menubar/Menubar.js";
 
-const navigationOpenner = document.querySelector('.button--category');
-const navigation = document.querySelector('.navigation__list');
+const navigationOpenner = document.querySelector(".button--category");
+const navigation = document.querySelector(".navigation__list");
 
 const menubar = new Menubar(navigation, {
   openner: navigationOpenner,
